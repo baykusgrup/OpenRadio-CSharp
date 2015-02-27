9fb3efd3998fab3b548a01eb0d132e8977efbed1
serbayacar <serbayacar@gmail.com>
2015-02-28 01:16:00 +0200

    on github
    Version 1

---
+++ OpenRadioCSharp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace OpenRadioCSharp
+{
+    partial class OpenRadio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpenRadio));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(327, 45);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(327, 187);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // OpenRadio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 256);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "OpenRadio";
+            this.Text = "OpenRadio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ OpenRadioCSharp/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenRadioCSharp
+{
+    public partial class OpenRadio : Form
+    {
+        public OpenRadio()
+        {
+            InitializeComponent();
+        }
+
+        string path = Application.StartupPath + "\\files\\urls.xlsx";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string yolBaglantisi = string.Concat("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=", path, ";Extended Properties='Excel 12.0 XML;HDR=YES;';");
+            OleDbConnection baglanti = new OleDbConnection(yolBaglantisi);
+            baglanti.Open();
+            OleDbDataAdapter veriAdaptor = new OleDbDataAdapter(@"SELECT * FROM [Sheet1$]", baglanti);
+
+            DataTable tablo = new DataTable();
+
+            veriAdaptor.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Seçili satırın X koordinatı
+
+            int xkoordinat = dataGridView1.CurrentCellAddress.X;
+
+            //Seçili satırın Y koordinatı
+
+            int ykoordinat = dataGridView1.CurrentCellAddress.Y;
+
+            string str = "";
+
+            str = dataGridView1.Rows[ykoordinat].Cells[xkoordinat].Value.ToString();
+            axWindowsMediaPlayer1.URL = str;
+        }
+    }
+}
